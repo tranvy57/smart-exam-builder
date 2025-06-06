04608990b719721564c91a022ce502357a40baea
zy <tranvy.art@gmail.com>
2025-06-06 15:03:32 +0700

    init project BE

---
+++ BE/smart-exam-builder-be/src/main/java/iuh/fit/smartexambuilderbe/SmartExamBuilderBeApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.smartexambuilderbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExamBuilderBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartExamBuilderBeApplication.class, args);
+    }
+
+}
---
+++ BE/smart-exam-builder-be/src/test/java/iuh/fit/smartexambuilderbe/SmartExamBuilderBeApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.smartexambuilderbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExamBuilderBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
